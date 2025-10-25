ab92e7e5e04c73cb5c93fbe3c36de907a99a1265
kiniku-man <sergioortizagudo@gmail.com>
2025-10-25 18:29:17 +0200

    Más clases

--- tema1/clase01.py
+++ tema1/clase01.py
@@ -1,3 +1,22 @@
 # Comentario
 
 print ("Hola Mundo")
+
+length=10;ancho=40 # Sensitive case, ¡cuidado!.
+area=length*ancho
+print(area)
+
+mensaje= "Este mensaje ocupará más de una linea por ello \
+escribimos de esta manera la multimlínea."
+print(mensaje)
+
+resultado= 500 * 3 + \
+4 + 7
+
+print(resultado)
+
+print("Este mensaje también se puede" \
+"cortar por la mitad porque está entre paréntesis") # El paréntesis no es necesario al usar paréntesis
+
+print("Este mensaje también se puede",
+"cortar por la mitad porque está entre paréntesis")

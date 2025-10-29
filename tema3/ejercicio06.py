abf395a6c2ca31975a50ec653cd715c174db4eb2
sortiz12321 <sortiz@ieseljust.com>
2025-10-29 08:43:38 +0100

    update

--- tema3/ejercicio06.py
+++ tema3/ejercicio06.py
@@ -10,4 +10,17 @@ Rewrite Exercise 4 to count the frequency of each word in the user's input.
     Finally, print the results sorted by the counts.
 '''
 
-dd
+dato=input("Indique frase: ")
+lista=dict()    # Creamos un diccionario vacío
+for i in dato:
+    chivato=0
+    for x,y in lista.items():
+        if x == i:              # Si se encuentra la letra en el diccionario se marca chivato para no guardarla
+            chivato=1           # y se incrementa su valor.
+            y=y+1
+            lista[i]=y
+    if chivato == 1:
+        chivato=0
+    else:
+        lista[i]=1
+print(lista)
